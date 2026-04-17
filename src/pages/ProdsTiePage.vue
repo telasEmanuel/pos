621a100d51f8f61108c7c935611634b19c60d524
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2026-04-17 11:04:24 -0500

    feat: add 'Consultor' role to action buttons visibility in ProdsTiePage

--- src/pages/ProdsTiePage.vue
+++ src/pages/ProdsTiePage.vue
@@ -855,7 +855,7 @@ watch(
               </div>-->
 
               <!-- Action Buttons (Editar y Eliminar) -->
-              <div class="card-actions" v-if="datos?.rol === 'visor' || datos?.rol === 'asistente'">
+              <div class="card-actions" v-if="datos?.rol === 'visor' || datos?.rol === 'asistente' || datos?.rol === 'consultor'">
                 <button @click="abrirEditModal(prod)" class="btn-editar">Editar</button>
                 <button @click="eliminarInventario(prod.id)" class="btn-eliminar">Eliminar</button>
               </div>
